dcf20520b96260f128eb9cdca1fc094cfb5b4929
hidayahrah <hidayahrah97@gmail.com>
2022-11-06 00:56:56 +0800

    Added index.html

--- app.js
+++ app.js
@@ -10,19 +10,22 @@ app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
 app.use("/js", express.static(__dirname + "public/js"));
 app.use("/img", express.static(__dirname + "public/img"));
+app.engine("html", require("ejs").renderFile);
 
 // Set View's
 app.set("views", "./views");
-app.set("view engine", "ejs");
+app.set("view engine", "html");
+// app.set("views", "./views/v2");
+// app.set("view engine", "ejs");
 
 // Navigation
-app.get("", (req, res) => {
-  res.render("index", { text: "Hey" });
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", { text: "What about you, bitch?" });
-});
+// app.get("/about", (req, res) => {
+//   res.render("about", { text: "What about you, bitch?" });
+// });
 
 // Listen on Port 5000
 app.listen(port, () => console.info(`App listening on port ${port}`));
